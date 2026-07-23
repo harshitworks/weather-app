f969f186e12e1a10b7552ce7ba57b5aefd6db6e9
Harshit Gaur <gaurharshit480@gmail.com>
2026-07-23 23:11:30 +0530

    Add dynamic temperature

--- script.js
+++ script.js
@@ -38,3 +38,17 @@ function changeImage(el) {
 
 
 // }
+
+const temp = document.querySelector(".curr-temp");
+
+let baseurl = "https://api.open-meteo.com/v1/forecast?latitude=28.4962&longitude=77.536&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_probability_max&hourly=temperature_2m,rain,relative_humidity_2m,precipitation_probability,precipitation,weather_code&models=best_match&current=relative_humidity_2m,temperature_2m,apparent_temperature,wind_speed_10m,is_day&timezone=auto&wind_speed_unit=ms";
+
+async function call() {
+  let response = await fetch(baseurl);
+  let data = await response.json();
+  console.log(data);
+  let curr_temp = data.current.temperature_2m;
+  temp.innerText = `${curr_temp} °C`;
+
+}
+call();
