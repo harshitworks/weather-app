7d006cd14a2e6d7190f5a549c522fe9ec257e662
Harshit Gaur <gaurharshit480@gmail.com>
2026-07-31 17:37:19 +0530

    add the seach your city feature

--- script.js
+++ script.js
@@ -4,66 +4,97 @@ let dy = document.querySelector(".daylength");
 const timeElement = document.getElementById("time3");
 const rise = document.getElementById("time1");
 const set = document.getElementById("time2");
-
+const e5 = document.querySelector('#tomorrow-temp');
+const e6 = document.querySelector("#search-city");
+const e7 = document.querySelector(".fa-solid.fa-magnifying-glass");
+const e8 = document.querySelector("#text1");
 const temp = document.querySelector(".curr-temp");
+
 let lat = 28.49615;
 let lon = 77.53601;
-let baseurl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=sunset,sunrise,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,daylight_duration,wind_speed_10m_max&hourly=temperature_2m,precipitation_probability,apparent_temperature,relative_humidity_2m,precipitation,cloud_cover,visibility,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,wind_speed_10m,rain,precipitation,cloud_cover&timezone=auto`;
 let celsius = true;
 let currentTempC = null;
 
-async function coordinates() {
-  const city = "Bulandshahr";
+async function coordinates(city) {
   const geoUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`;
+  try {
+          const response = await fetch(geoUrl);
 
-  const response = await fetch(geoUrl);
-  const data = await response.json();
-  const lat = data.results[0].latitude;
-  const lon = data.results[0].longitude;
+          if (!response.ok) {
+              throw new Error(`HTTP Error: ${response.status}`);
+          }
 
-  return {
-    lat: lat,
-    lon : lon
-  }
+          const data = await response.json();
+
+          let check = false;
+          if (!data.results || data.results.length === 0) {
+            lat = 28.49615;
+            lon = 77.53601;
+          }
+          else {
+            lat = data.results[0].latitude ;
+            lon = data.results[0].longitude;
+            check = true;
+          }
+          
+          return {
+            lati: lat,
+            loni : lon,
+            value : check
+          }
+
+      } catch (err) {
+          console.error("Error:", err.message);
+      }
+
 }
 
-async function call() {
-  const ans = await coordinates();
-  lat = ans.lat;
-  lon = ans.lon;
-
-  baseurl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=sunset,sunrise,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,daylight_duration,wind_speed_10m_max&hourly=temperature_2m,precipitation_probability,apparent_temperature,relative_humidity_2m,precipitation,cloud_cover,visibility,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,wind_speed_10m,rain,precipitation,cloud_cover&timezone=auto`;
-
+async function call(city) {
+  const ans = await coordinates(city);
+    lat = ans.lati;
+    lon = ans.loni;
+    let value = ans.value;
+  
+  const baseurl = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=sunset,sunrise,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,daylight_duration,wind_speed_10m_max&hourly=temperature_2m,precipitation_probability,apparent_temperature,relative_humidity_2m,precipitation,cloud_cover,visibility,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,wind_speed_10m,rain,precipitation,cloud_cover&timezone=auto`;
 
   let response = await fetch(baseurl);
   let data = await response.json();
-
   console.log(data);
   setTemperature(data);
   sunTime(data);
   nextFive(data);
-  
-  
+  return value;
   
 }
+
 // Dynamic City
 
-// const e6 = document.querySelector("#search-city");
-// e6.addEventListener("keydown", (evt)=>{searchCity(evt);})
+e6.addEventListener("keydown", (evt)=>{searchCity(evt);})
 
-// const e7 = document.querySelector(".fa-solid.fa-magnifying-glass");
-
-// e7.addEventListener("click", (evt) => {
-//     console.log(e6.value);
-// });
-
-
-// function searchCity(evt) {
-//   if(evt.key=='Enter') {
-//     console.log(e6.value);
-//   }
+e7.addEventListener("click", async (evt) => {
+    cityFind();
+  });
   
-// }
+  async function searchCity(evt) {
+    if(evt.key=='Enter') {
+      cityFind();
+    }
+  }
+  
+  async function cityFind(){
+  let city = e6.value;
+  if(city.length<=2) {
+    city = "Noida";
+  }
+  let value = await call(city);
+  if(!value) {
+    city = "Noida";
+  }
+  city = city.charAt(0).toUpperCase() + city.slice(1)
+  e8.innerText= city;
+  e6.value = "";
+
+}
 
 function nextFive(data){
   const times = data.hourly.time;
@@ -97,12 +128,8 @@ function nextFive(data){
     }
     
     // tomorrow 
-    const e5 = document.querySelector('#tomorrow-temp');
     let avg = ((data.daily.temperature_2m_max[1] + data.daily.temperature_2m_min[1])/2).toFixed(2) ;
     e5.innerText= avg+" °C";
-    
-    console.log(avg);
-
 
 }
 
@@ -128,9 +155,6 @@ function sunTime(data) {
 
 }
 
-window.addEventListener("load", async () => {
-  await call();
-});
 
 fbtn.addEventListener("click", () => {
   fbtn.classList.add("toggle-btn");
@@ -159,13 +183,18 @@ img.addEventListener("click", (evt) => changeImage(evt.currentTarget));
 function changeImage(el) {
   const currentUrl = getComputedStyle(el).backgroundImage;
   const currentName = currentUrl
-    .split("/")
-    .pop()
-    .replace(/["')]/g, "")
-    .replace(/\.[^.]+$/, "");
+  .split("/")
+  .pop()
+  .replace(/["')]/g, "")
+  .replace(/\.[^.]+$/, "");
   const currentIndex = imgArr.indexOf(currentName);
-  const nextIndex = currentIndex >= 0 ? (currentIndex + 1) % imgArr.length : 0;
-
+  const nextIndex = currentIndex >= 0 ? (currentIndex + 1) % imgArr.length : 0; 
   const nextName = imgArr[nextIndex];
   el.style.backgroundImage = `url("images/${nextName}.png")`;
 }
+
+
+window.addEventListener("load", async () => {
+  e8.innerText= "Noida";
+  await call("Noida");
+});
